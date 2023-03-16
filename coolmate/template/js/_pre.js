23a6d7bc023796f6086bb34eb2edf4bf6786d48a
chihieu0112 <hieulau0112@gmail.com>
2023-03-16 12:47:15 +0700

    init blog page template

---
+++ coolmate/template/js/_pre.js
@@ -0,0 +1,6 @@
+window.theme=window.theme||{}
+window.cm=window.cm||{}
+theme.setCookie=function(cname,cvalue,exdays){var d=new Date();d.setTime(d.getTime()+(exdays*24*60*60*1000));var expires="expires="+d.toUTCString();document.cookie=cname+"="+cvalue+";"+expires+";path=/";}
+theme.getCookie=function(cname){var name=cname+"=";var decodedCookie=decodeURIComponent(document.cookie);var ca=decodedCookie.split(';');for(var i=0;i<ca.length;i++){var c=ca[i];while(c.charAt(0)==' '){c=c.substring(1);}
+if(c.indexOf(name)==0){return c.substring(name.length,c.length);}}
+return ""}
